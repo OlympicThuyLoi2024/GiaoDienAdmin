80581605c40c995fd6c297f5ec722b1ed61cef3e
ThanhNL <120170567+trungthanhcva2206@users.noreply.github.com>
2024-12-09 07:13:16 +0700

    Update NavigationFunctions.js

--- pages/Sign in/jsobjects/NavigationFunctions/NavigationFunctions.js
+++ pages/Sign in/jsobjects/NavigationFunctions/NavigationFunctions.js
@@ -17,3 +17,9 @@ export default {
         navigateTo(targetAppURL, 'SAME_WINDOW');
     }
 }
+/**
+ * @Project GiaoDienAdmin
+ * @Author TT(trungthanhcva2206@gmail.com@gmail.com)
+ * @Copyright (C) 2024 CHK. All rights reserved
+ * @License GNU/GPL version 3.0
+ */
