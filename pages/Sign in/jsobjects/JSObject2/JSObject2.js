0f1a47b41348a00a66c395eac098c7f956f115fe
ThanhNL <120170567+trungthanhcva2206@users.noreply.github.com>
2024-12-09 07:12:53 +0700

    Update JSObject2 Login

--- pages/Sign in/jsobjects/JSObject2/JSObject2.js
+++ pages/Sign in/jsobjects/JSObject2/JSObject2.js
@@ -1,1 +1,7 @@
 export default { setRoleId: () => { storeValue('roleId', 'brfrz0brwjcxqmbqaut8se0c'); } }
+/**
+ * @Project GiaoDienAdmin
+ * @Author TT(trungthanhcva2206@gmail.com@gmail.com)
+ * @Copyright (C) 2024 CHK. All rights reserved
+ * @License GNU/GPL version 3.0
+ */
