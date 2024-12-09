6af5751523e9f30e321d177492ef98a77f181203
ThanhNL <120170567+trungthanhcva2206@users.noreply.github.com>
2024-12-09 07:12:17 +0700

    Update JSObject1 Login

--- pages/Sign in/jsobjects/JSObject1/JSObject1.js
+++ pages/Sign in/jsobjects/JSObject1/JSObject1.js
@@ -6,3 +6,9 @@ export default {
         storeValue("selectedTab", "Sign_Up");
     }
 }
+/**
+ * @Project GiaoDienAdmin
+ * @Author TT(trungthanhcva2206@gmail.com@gmail.com)
+ * @Copyright (C) 2024 CHK. All rights reserved
+ * @License GNU/GPL version 3.0
+ */
